f0c49699c60e3ea4c09bc8f8aac9a279575755e1
unknown <eddy@Surface-02.ebanan.local>
2022-07-05 09:56:24 +0200

    first

---
+++ deck.js
@@ -0,0 +1,77 @@
+const SUITS = ["♠", "♣", "♥", "♦"];
+const VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
+
+export default class Deck {
+  constructor(cards = freshDeck()) {
+    this.cards = cards;
+  }
+
+  get numberOfCards() {
+    return this.cards.length;
+  }
+
+  pop() {
+    return this.cards.shift();
+  }
+
+  push(card) {
+    this.cards.push(card);
+  }
+
+  //using a loof instead of sort because the latter is used for sorting in some order
+
+  // the function needs to get a random index then swap it with the card thats we already on
+
+  shuffle() {
+    for (let i = this.numberOfCards - 1; i > 0; i--) {
+      const newIndex = Math.floor(Math.random() * (i + 1));
+      const oldValue = this.cards[newIndex];
+      this.cards[newIndex] = this.cards[i];
+      this.cards[i] = oldValue;
+    }
+  }
+}
+
+class Card {
+  constructor(suit, value) {
+    this.suit = suit;
+    this.value = value;
+  }
+
+  get color() {
+    return this.suit === "♣" || this.suit === "♠" ? "black" : "red";
+  }
+
+  getHTML() {
+    const cardDiv = document.createElement("div");
+    cardDiv.innerText = this.suit;
+    cardDiv.classList.add("card", this.color);
+    cardDiv.dataset.value = `${this.value} ${this.suit}`;
+    return cardDiv;
+  }
+}
+
+// this function is gonna create a fresh deck by looping through the suits and valuess
+//using flat.map instead of map to combine the arrays into one
+
+function freshDeck() {
+  return SUITS.flatMap((suit) => {
+    return VALUES.map((value) => {
+      return new Card(suit, value);
+    });
+  });
+}
